5fcc490ac7b679765b9535ea40c542cb858814a2
Mick Welisevich <michael@welisevich.com>
2021-06-18 13:08:25 +0100

    Fixed file structure and added two program drafts.

---
+++ module_5/Untitled-1.py
@@ -0,0 +1,6 @@
+"""
+Mick Welisevich
+18 JUN 2021
+Module 5 Assignment 3
+"""
+
---
+++ module_5/mongodb_test.py
@@ -0,0 +1,22 @@
+"""
+Mick Welisevich
+17 JUN 2021
+Module 5 Assignment 2
+"""
+
+from pymongo import MongoClient
+#Step 1
+
+url = "mongodb+srv://admin:admin@cluster0.t6bdf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+#Step 2
+
+client = MongoClient(url)
+#Step 3
+
+db = client.pytech
+#Step 4
+
+print("-- Pytech Collection List --")
+print(db.list_collection_names())
+print("\nEnd of program, press any key to exit...")
+#Step 5
